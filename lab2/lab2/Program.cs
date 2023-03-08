9b11df6c67f3c30e7b801d02a70be97f6542ec3d
Negven <35338243+Negven@users.noreply.github.com>
2023-03-08 12:37:37 +0100

    tasl2_2

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -64,13 +64,13 @@ namespace lab2
             }
         }
         
-        public static void outArray(int a, int b, ref int[,] matrix)
+        public static void outArray(int a, int b, int[,] matrix)
         {
             for (int x = 0; x < a; x++)
             {
                 for (int y = 0; y < b; y++)
                 {
-                    Console.Write(matrix[y, x] + " ");
+                    Console.Write(matrix[x, y] + " "); //return y, x
                 }
                 Console.WriteLine();
             }
@@ -165,7 +165,7 @@ namespace lab2
             Console.Write("Enter k: ");
             int k = Convert.ToInt32(Console.ReadLine());
             
-            outArray(a, b, ref matrix);
+            outArray(a, b, matrix);
 
             for (int x = 1; x < a; x += 2)
             {
@@ -176,7 +176,109 @@ namespace lab2
                 }
             }
             Console.WriteLine("--------");
-            outArray(a, b, ref matrixNew);
+            outArray(a, b, matrixNew);
+        }
+
+        public static bool isLineIsZero(int[,] matrix, int b, int numberLine)
+        {
+            bool answer = true;
+            for (int y = 0; y < b; y++)
+            {
+                if (matrix[numberLine, y] != 0) answer = false;
+            }
+
+            return answer;
+        }
+        
+        public static bool isColumnIsZero(int[,] matrix, int a, int numberColumn)
+        {
+            bool answer = true;
+            for (int x = 0; x < a; x++)
+            {
+                if (matrix[x, numberColumn] != 0) answer = false;
+            }
+
+            return answer;
+        }
+
+        public static void TransposeMatrix(ref int[,] matrix, ref int a, ref int b)
+        {
+            int[,] result = new int[b, a];
+            for (int x = 0; x < b; x++)
+            {
+                for (int y = 0; y < a; y++)
+                {
+                    result[x, y] = matrix[y, x];
+                }
+            }
+            Swap(ref a, ref b);
+            matrix = result;
+        }
+        
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T c;
+            c = a;
+            a = b;
+            b = c;
+        }
+
+        public static void deleteZeroLines(ref int[,] matrix, ref int a, int b)
+        {
+            for (int x = 0; x < a; x++)
+            {
+                if (isLineIsZero(matrix, b, x))
+                {
+                    for (int xNew = x; xNew < a - 1; xNew++)
+                    {
+                        for (int y = 0; y < b; y++)
+                        {
+                            matrix[xNew, y] = matrix[xNew + 1, y];
+                        }
+                    }
+
+                    a--;
+                }
+            }
+        }
+
+        public static void task2_2()
+        {
+            Console.Write("Enter a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix = new int[a, b];
+            enterArray(a, b, ref matrix);
+
+            // int[,] matrix = new int[5, 5]
+            // {
+            //     { 1, 0, 2, 0, 4, },
+            //     { 2, 0, 3, 0, 4,},
+            //     { 3, 0, 4, 0, 6, },
+            //     { 4, 0, 2, 0, 5, },
+            //     { 2, 0, 3, 0, 4,}
+            //
+            // };
+            
+            // int[,] matrix = new int[5, 5]
+            // {
+            //     { 1, 2, 2, 3, 4, },
+            //     { 0, 0, 0, 0, 0,},
+            //     { 3, 0, 4, 0, 6, },
+            //     { 0, 0, 0, 0, 0, },
+            //     { 2, 4, 3, 4, 4,}
+            //
+            // };
+            outArray(a, b, matrix);
+            Console.WriteLine("--------");
+            deleteZeroLines(ref matrix, ref a, b);
+            TransposeMatrix(ref matrix, ref a, ref b);
+            deleteZeroLines(ref matrix, ref a, b);
+            TransposeMatrix(ref matrix, ref a, ref b);
+            outArray(a, b, matrix);
+
         }
         
         public static void Main(string[] args)
@@ -185,6 +287,9 @@ namespace lab2
             task1_2();
             task1_3();   
             task2_1();
+            task2_2();
+
+
         }
     }   
 }
