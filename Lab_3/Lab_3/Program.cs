b82df2ac1c15297c86d7367fc1169862b1ef1615
Negven <35338243+Negven@users.noreply.github.com>
2023-04-16 22:37:36 +0200

    feat: add menu

--- Lab_3/Lab_3/Program.cs
+++ Lab_3/Lab_3/Program.cs
@@ -7,8 +7,41 @@ namespace Lab_3
     {
         public static void Main(string[] args)
         {
-            ArrTwoSpace arrOne = new ArrTwoSpace(new  int[2, 4]{ { 3, 5, -3, 4 }, {10, 2, 3, 2}});
-            Console.WriteLine(arrOne.sumMinMax());
+            Console.Write("Enter the dimensionality of the array: ");
+            int numberSpace = Convert.ToInt32(Console.ReadLine());
+            if (numberSpace == 1)
+            {
+                Console.Write("Enter n: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] arr = new int [n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(i + ". ");
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                ArrOneSpace arrOne = new ArrOneSpace(arr);
+                Console.WriteLine(arrOne.sumMinMax());
+            }
+
+            if (numberSpace == 2)
+            {
+                Console.Write("Enter n: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter m: ");
+                int m = Convert.ToInt32(Console.ReadLine());
+                int[,] arr = new int [n,m];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int k = 0; k < m; k++) {
+                        Console.Write(i + ", " + k + ": ");
+                        arr[i, k] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+
+                ArrTwoSpace arrOne = new ArrTwoSpace(arr);
+                Console.WriteLine(arrOne.sumMinMax());
+            }
         }
     }
 }
