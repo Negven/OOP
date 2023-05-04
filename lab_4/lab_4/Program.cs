190a2da3a3e94e83714860a3e49ba4c3ef66c8b8
Negven <35338243+Negven@users.noreply.github.com>
2023-05-05 01:54:16 +0200

    fix: add test for length

--- lab_4/lab_4/Program.cs
+++ lab_4/lab_4/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Ports;
+using System.Linq;
 
 namespace lab_4
 {
     internal class Program
     {
+        public static readonly char[] alphabet = new [] {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
 
+        public static readonly string[] shortWords = new[]
+        {
+            "do", "is", "no", "we", "go"
+        };
+
+        public static List<string> splitTextByWords(char[] text)
+        {
+            List<string> result = new List<string>();
+            int indexText = 0;
+            while (indexText < text.Length)
+            {
+                string word = "";
+                while (indexText < text.Length && alphabet.Contains(text[indexText]))
+                {
+                    word += text[indexText];
+                    indexText++;
+                }
+                if(word.Length > 2) result.Add(word);
+                if (word.Length == 2)
+                {
+                    if(shortWords.Contains(word)) result.Add(word);
+                }
+                indexText++;
+            }
+
+            return result;
+        }
+
+        public static int countWordsByLetters(char[] text)
+        {
+            List<string> splitText = splitTextByWords(text);
+            foreach (var word in splitText)
+            {
+                Console.WriteLine(word);
+            }
+
+            return splitText.Count;
+        }
+        
         public static int countWords(char[] text)
         {
             int counter = 0;
@@ -56,17 +100,15 @@ namespace lab_4
         public static void Main(string[] args)
         {
             string text = File.ReadAllText($@"{Environment.CurrentDirectory}\..\..\..\text.txt");
-            Console.WriteLine("---------------");
-            Console.WriteLine($@"{Environment.CurrentDirectory}\text.txt");
-            Console.WriteLine("---------------");
             char[] symbols = text.ToCharArray();
             List<char> symbolsList = new List<char>();
-            foreach (var sym in text)
-            {
-               symbolsList.Add(sym);
-            }
-            Console.WriteLine(countWords(symbols));
-            Console.WriteLine(countWords(symbolsList));
+            // foreach (var sym in text)
+            // {
+            //    symbolsList.Add(sym);
+            // }
+            // Console.WriteLine(countWords(symbols));
+            // Console.WriteLine(countWords(symbolsList));
+            Console.WriteLine(countWordsByLetters(symbols));
         }
     }
 }
